6e5edb1a5f943f5605df2e4120db66830a179452
Sanshiroh SAKAI <sansyo@gmail.com>
2024-01-28 16:37:38 +0900

    API ルートを定義

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import NextAuth, { type NextAuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID ?? '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+    }),
+  ],
+}
+
+// NextAuth.js を初期化
+// NextAuth 関数に初期化オプションを渡すことで認証機能を提供するハンドラ関数を返す
+const handler = NextAuth(authOptions)
+
+// NextAuth の認証機能を処理するためのエンドポイントとして利用する関数をエクスポート
+export { handler as GET, handler as POST }
